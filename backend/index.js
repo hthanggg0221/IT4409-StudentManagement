329f7e80fe413a6298df6959fd62f7f060c4c73c
hthanggg0221 <hoangvanthang.work@gmail.com>
2025-12-06 22:50:12 +0700

    Bai 2

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,29 @@ app.get('/api/students', async (req, res) => {
     }
 });
 
+app.post('/api/students', async (req, res) => {
+    try {
+        const newStudent = await Student.create(req.body);
+        res.status(201).json(newStudent);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
+});
+
+app.put('/api/students/:id', async (req, res) => {
+    try {
+        const updatedStu = await Student.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        if (!updatedStu) return res.status(404).json({ error: "Student not found" });
+        res.json(updatedStu);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
